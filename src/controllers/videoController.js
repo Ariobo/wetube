043a510a1cd4688e9d5a0f1cd944773fbafd6f1e
hajin <dhflgkwls87@gmail.com>
2022-08-01 00:50:56 +0900

    fun

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,34 @@
-export const trending=(req,res)=>res.render("home");
+
+export const trending = (req, res) => {
+    const videos = [
+      {
+        title: "one",
+        rating: 5,
+        Comments: 2,
+        createdAt: "2 minutes ago",
+        views: 50,
+        id: 1,
+      },
+      {
+        title: "two",
+        rating: 5,
+        Comments: 2,
+        createdAt: "2 minutes ago",
+        views: 50,
+        id: 1,
+      },
+      {
+        title: "there",
+        rating: 5,
+        Comments: 2,
+        createdAt: "2 minutes ago",
+        views: 50,
+        id: 1,
+      },
+      
+    ];
+    return res.render("home", { pageTitle: "Home", videos });
+};
 export const see=(req,res)=>res.render("watch");
 export const edit=(req,res)=> res.send("edit");
 export const search=(req,res)=>res.send("Search");
