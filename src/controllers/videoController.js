14653c2eac5ec1c35d0f7b3af689d847534c4c5c
hajin <dhflgkwls87@gmail.com>
2022-08-06 14:44:48 +0900

    0805

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -38,4 +38,27 @@ export const getEdit=(req,res)=> {
   const video=videos[id-1];
   return res.render("edit",{ pageTitle:`Editing: ${video.title}`,video});
 };
-export const postEdit=(req,res)=>{};
+export const postEdit=(req,res)=>{
+  const { id }=req.params;
+  const{ title }=req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload=(req,res)=>{
+  return res.render("Upload",{ pageTitle: "Upload Video"});
+};
+
+export const postUpload=(req,res)=>{
+  const{title}=req.body;
+  const newVideo={
+    title,
+    rating: 0,
+    Comments: 0,
+    createdAt: "Just now",
+    views: 0,
+    id:videos.length+1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+}
