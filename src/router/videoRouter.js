fc3fc9d20740d936bd30b59e0cb87510a911707b
hajin <dhflgkwls87@gmail.com>
2022-09-11 01:11:29 +0900

    asd

--- src/router/videoRouter.js
+++ src/router/videoRouter.js
@@ -7,5 +7,4 @@ videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
-
 export default videoRouter;
