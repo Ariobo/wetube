d612437232bed05145fdbfe09221c31333ed3691
hajin <dhflgkwls87@gmail.com>
2022-09-24 21:46:24 +0900

    api github
    왜 안됨?

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,9 @@ import bcrypt from "bcrypt";
 
 const userSchema =new mongoose.Schema({
     email:{type:String,require:true,unique:true},
+    socialOnly: { type: Boolean, default: false },
     username:{type:String,require:true,unique:true},
-    password:{type:String,require:true},
+    password:{type:String},
     name:{type:String,require:true},
     location: String,
 });
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import session from "express-session";
 import MongoStore from "connect-mongo";
 import rootRouter from "./router/rootRouter";
 import userRouter from "./router/userRouter";
-import videoRouter from "./router/videoRouter";
+import videoRouter from "./router/videoRouter"; 
 import { localeMiddleware } from "./middlewares";
 
 const app = express();
@@ -16,10 +16,10 @@ app.use(logger);
 app.use(express.urlencoded({extended:true}))
 
 app.use(session({
-    secret: "hello",
-    resave: true,
-    saveUninitialized: true,
-    store: MongoStore.create({mongoUrl:"mongodb://127.0.0.1:27017/wetube"}),
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl:process.env.DB_URL}),
 }))
 
 app.use((req,res,next) => {
